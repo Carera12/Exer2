7d560dba2643a597d6e20b16616102a6dfe06c4e
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-10-31 16:07:26 +0700

    add while di public void untuk menambah text yang berguna untuk memasukan jumlah element dalam array yang berminimal 1 dan maximum 83

--- Exer2/Program.cs
+++ Exer2/Program.cs
@@ -11,6 +11,20 @@ namespace Exer2
         int[] Rera = new int[83];
         int n;
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 83))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+        }
         static void Main(string[] args)
         {
         }
